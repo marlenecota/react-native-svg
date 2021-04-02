4235a1f5520ef6d4a21da6ab8f8e58ff0b2b83f9
Marlene Cota <mcota@microsoft.com>
2021-04-02 15:41:33 -0700

    Add BrushView + Implement LinearGradientView

--- windows/RNSVG/DefsView.cpp
+++ windows/RNSVG/DefsView.cpp
@@ -6,15 +6,5 @@ using namespace winrt;
 using namespace Microsoft::Graphics::Canvas;
 
 namespace winrt::RNSVG::implementation {
-void DefsView::Render(UI::Xaml::CanvasControl const &/*canvas*/, CanvasDrawingSession const &/*session*/) {
-  if (auto const &root{SvgRoot()}) {
-    for (auto const &child : Children()) {
-      if (auto const &defView{child.try_as<IDefinitionView>()}) {
-        defView.SaveDefinition();
-      } else if (child.Id() != L"") {
-        root.Templates().Insert(child.Id(), child);
-      }
-    }
-  }
-}
+void DefsView::Render(UI::Xaml::CanvasControl const &/*canvas*/, CanvasDrawingSession const &/*session*/) {}
 } // namespace winrt::RNSVG::implementation
--- windows/RNSVG/LinearGradientView.h
+++ windows/RNSVG/LinearGradientView.h
@@ -1,27 +1,28 @@
 #pragma once
 #include "LinearGradientView.g.h"
-#include "GroupView.h"
+#include "BrushView.h"
 
 namespace winrt::RNSVG::implementation {
-struct LinearGradientView : LinearGradientViewT<LinearGradientView, RNSVG::implementation::GroupView> {
+struct LinearGradientView : LinearGradientViewT<LinearGradientView, RNSVG::implementation::BrushView> {
  public:
   LinearGradientView() = default;
 
-  // IDefinitionView
-  void SaveDefinition();
-  void RemoveDefinition();
-
   // RenderableView
   void UpdateProperties(Microsoft::ReactNative::IJSValueReader const &reader, bool forceUpdate, bool invalidate);
-  void Render(
-      Microsoft::Graphics::Canvas::UI::Xaml::CanvasControl const &canvas,
-      Microsoft::Graphics::Canvas::CanvasDrawingSession const &session);
 
  private:
   RNSVG::SVGLength m_x1{};
   RNSVG::SVGLength m_y1{};
   RNSVG::SVGLength m_x2{};
   RNSVG::SVGLength m_y2{};
+  std::vector<Microsoft::Graphics::Canvas::Brushes::CanvasGradientStop> m_stops{};
+  std::string m_gradientUnits{"objectBoundingBox"};
+  bool m_transformSet{false};
+  Numerics::float3x2 m_transform{Numerics::make_float3x2_scale(1)};
+
+  void CreateBrush();
+  void UpdateBounds();
+  void SetPoints(Microsoft::Graphics::Canvas::Brushes::CanvasLinearGradientBrush brush, Windows::Foundation::Rect const &bounds);
 };
 } // namespace winrt::RNSVG::implementation
 
