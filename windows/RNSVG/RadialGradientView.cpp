dc159633f62093114ff271257fc13a0502f87232
REDMOND\agnel <agnel@microsoft.com>
2021-04-11 19:54:37 -0500

    starter radviews

--- windows/RNSVG/RadialGradientView.cpp
+++ windows/RNSVG/RadialGradientView.cpp
@@ -2,6 +2,54 @@
 #include "RadialGradientView.h"
 #include "RadialGradientView.g.cpp"
 
-namespace winrt::RNSVG::implementation
-{
+#include "Utils.h"
+
+using namespace winrt;
+using namespace Microsoft::Graphics::Canvas;
+using namespace Microsoft::ReactNative;
+
+namespace winrt::RNSVG::implementation {
+void RadialGradientView::UpdateProperties(IJSValueReader const &reader, bool forceUpdate, bool invalidate) {
+  const JSValueObject &propertyMap{JSValue::ReadObjectFrom(reader)};
+
+  for (auto const &pair : propertyMap) {
+    auto const &propertyName{pair.first};
+    auto const &propertyValue{pair.second};
+
+    if (propertyName == "fx") {
+      m_fx = SVGLength::From(propertyValue);
+    } else if (propertyName == "fy") {
+      m_fy = SVGLength::From(propertyValue);
+    } else if (propertyName == "rx") {
+      m_rx = SVGLength::From(propertyValue);
+    } else if (propertyName == "ry") {
+      m_ry = SVGLength::From(propertyValue);
+    } else if (propertyName == "cx") {
+      m_cx = SVGLength::From(propertyValue);
+    } else if (propertyName == "cy") {
+      m_cy = SVGLength::From(propertyValue);
+    } else if (propertyName == "gradient") {
+      m_stops = Utils::JSValueAsStops(propertyValue);
+    } else if (propertyName == "gradientUnits") {
+      m_gradientUnits = Utils::JSValueAsBrushUnits(propertyValue);
+    } else if (propertyName == "gradientTransform") {
+      m_transformSet = true;
+      m_transform = Utils::JSValueAsTransform(propertyValue);
+
+      if (propertyValue.IsNull()) {
+        m_transformSet = false;
+      }
+    }
+  }
+
+  __super::UpdateProperties(reader, forceUpdate, invalidate);
+
+  SaveDefinition();
 }
+
+void RadialGradientView::Unload() {
+  m_stops.clear();
+  __super::Unload();
+}
+
+} // namespace winrt::RNSVG::implementation
--- windows/RNSVG/RadialGradientView.h
+++ windows/RNSVG/RadialGradientView.h
@@ -2,17 +2,29 @@
 #include "RadialGradientView.g.h"
 #include "BrushView.h"
 
-namespace winrt::RNSVG::implementation
-{
-    struct RadialGradientView : RadialGradientViewT<RadialGradientView, RNSVG::implementation::BrushView>
-    {
-        RadialGradientView() = default;
+namespace winrt::RNSVG::implementation {
+struct RadialGradientView : RadialGradientViewT<RadialGradientView, RNSVG::implementation::BrushView> {
+ public:
+  RadialGradientView() = default;
 
-    };
-}
-namespace winrt::RNSVG::factory_implementation
-{
-    struct RadialGradientView : RadialGradientViewT<RadialGradientView, implementation::RadialGradientView>
-    {
-    };
-}
+  // RenderableView
+  void UpdateProperties(Microsoft::ReactNative::IJSValueReader const &reader, bool forceUpdate, bool invalidate);
+  void Unload();
+
+ private:
+  RNSVG::SVGLength m_fx{};
+  RNSVG::SVGLength m_fy{};
+  RNSVG::SVGLength m_rx{};
+  RNSVG::SVGLength m_ry{};
+  RNSVG::SVGLength m_cx{};
+  RNSVG::SVGLength m_cy{};
+  std::vector<Microsoft::Graphics::Canvas::Brushes::CanvasGradientStop> m_stops{};
+  std::string m_gradientUnits{"objectBoundingBox"};
+  bool m_transformSet{false};
+  Numerics::float3x2 m_transform{Numerics::make_float3x2_scale(1)};
+};
+} // namespace winrt::RNSVG::implementation
+
+namespace winrt::RNSVG::factory_implementation {
+struct RadialGradientView : RadialGradientViewT<RadialGradientView, implementation::RadialGradientView> {};
+} // namespace winrt::RNSVG::factory_implementation
