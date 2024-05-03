16487b045dd4ab706630ce7f1b15bc267eccc3c1
Marlene Cota <mcota@microsoft.com>
2024-05-02 17:20:16 -0700

    add template windows FabricExample

--- FabricExample/metro.config.js
+++ FabricExample/metro.config.js
@@ -9,6 +9,7 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
 /* eslint-disable import/no-commonjs */
 
+const fs = require('fs');
 const path = require('path');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
 const escape = require('escape-string-regexp');
@@ -20,18 +21,30 @@ const modules = [
   ...Object.keys(pack.peerDependencies),
 ];
 
+const rnwPath = fs.realpathSync(
+  path.resolve(require.resolve('react-native-windows/package.json'), '..'),
+);
 const config = {
   projectRoot: __dirname,
   watchFolders: [root],
 
   // We need to make sure that only one version is loaded for peerDependencies
-  // So we exclude them at the root, and alias them to the versions in example's node_modules
+  // So we block them at the root, and alias them to the versions in example's node_modules
   resolver: {
     blacklistRE: exclusionList(
       modules.map(
         (m) =>
-          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`),
-      ),
+          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
+      ).concat([
+        // This stops "react-native run-windows" from causing the metro server to crash if its already running
+        new RegExp(
+          `${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`,
+        ),
+        // This prevents "react-native run-windows" from hitting: EBUSY: resource busy or locked, open msbuild.ProjectImports.zip or other files produced by msbuild
+        new RegExp(`${rnwPath}/build/.*`),
+        new RegExp(`${rnwPath}/target/.*`),
+        /.*\.ProjectImports\.zip/,
+      ])
     ),
 
     extraNodeModules: modules.reduce((acc, name) => {
